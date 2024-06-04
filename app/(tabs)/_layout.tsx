8afc21100d3eb556c0c6a1a3a04360e001961584
Ana Clara <126925400+littlehopw@users.noreply.github.com>
2024-06-03 23:25:46 -0300

    Update de acordo com o trabalho (adição do ícone da última página)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="sccp"
+        options={{
+          title: 'SCCP',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'key' : 'key-outline'} color={color} />
+          ),
+        }}
+      />  
     </Tabs>
   );
 }
